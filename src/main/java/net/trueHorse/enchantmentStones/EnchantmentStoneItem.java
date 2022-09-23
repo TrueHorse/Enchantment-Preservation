52af17202b2517a9ff16bad9f25d0a8a8b0059bb
Paul Roß <81418739+TrueHorse@users.noreply.github.com>
2022-09-23 13:15:17 +0200

    added enchantment stone item

---
+++ src/main/java/net/trueHorse/enchantmentStones/EnchantmentStoneItem.java
@@ -0,0 +1,10 @@
+package net.trueHorse.enchantmentStones;
+
+import net.minecraft.item.Item;
+
+public class EnchantmentStoneItem extends Item {
+
+    public EnchantmentStoneItem(Settings settings){
+        super(settings);
+    }
+}
--- src/main/java/net/trueHorse/enchantmentStones/EnchantmentStones.java
+++ src/main/java/net/trueHorse/enchantmentStones/EnchantmentStones.java
@@ -1,17 +1,26 @@
 package net.trueHorse.enchantmentStones;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EnchantmentStones implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+
 	public static final Logger LOGGER = LoggerFactory.getLogger("enchantmentstones");
+
+	public static final ItemGroup ENCHANTMENT_STONES_GROUP = FabricItemGroupBuilder.create(new Identifier("enchantmentstones","main_group")).icon(()->new ItemStack(Items.LAPIS_LAZULI)).build();
+	public static final Item ENCHANTMENT_STONE = new EnchantmentStoneItem(new FabricItemSettings().group(ENCHANTMENT_STONES_GROUP));
 
 	@Override
 	public void onInitialize() {
-
+		Registry.register(Registry.ITEM,new Identifier("enchantmentstones","enchantment_stone"),ENCHANTMENT_STONE);
 	}
 }
