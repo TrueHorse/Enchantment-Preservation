88c64a570949fe92bedc255c3b98311d29167da0
Paul Roß <81418739+TrueHorse@users.noreply.github.com>
2022-10-06 14:50:53 +0200

    stones enchantable in survival

--- src/main/java/net/trueHorse/enchantmentStones/mixin/EnchantmentScreenHandlerMixin.java
+++ src/main/java/net/trueHorse/enchantmentStones/mixin/EnchantmentScreenHandlerMixin.java
@@ -1,23 +1,18 @@
 package net.trueHorse.enchantmentStones.mixin;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.screen.EnchantmentScreenHandler;
 import net.trueHorse.enchantmentStones.EnchantmentStones;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(EnchantmentScreenHandler.class)
 public abstract class EnchantmentScreenHandlerMixin {
 
-    @ModifyArg(method = "method_17410",at=@At(value = "INVOKE",target = "Lnet/minecraft/screen/EnchantmentScreenHandler;generateEnchantments(Lnet/minecraft/item/ItemStack;II)Ljava/util/List;"),index=0)
-    private ItemStack bookEnchantmentsIfEnchantmentStone(ItemStack stack){
-        return stack.isIn(EnchantmentStones.ENCHANTMENT_STONES) ? Items.BOOK.getDefaultStack() : stack;
-    }
-
-    @ModifyArg(method = "method_17411",at=@At(value = "INVOKE",target = "Lnet/minecraft/screen/EnchantmentScreenHandler;generateEnchantments(Lnet/minecraft/item/ItemStack;II)Ljava/util/List;"),index=0)
-    private ItemStack bookEnchantmentsIfEnchantmentStone2(ItemStack stack){
-        return stack.isIn(EnchantmentStones.ENCHANTMENT_STONES) ? Items.BOOK.getDefaultStack() : stack;
+    @Redirect(method = "generateEnchantments",at=@At(value = "INVOKE",target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"))
+    private boolean isOfBookOrEnchantmentStone(ItemStack instance, Item item){
+        return instance.isOf(item)||instance.isIn(EnchantmentStones.ENCHANTMENT_STONES);
     }
 }
